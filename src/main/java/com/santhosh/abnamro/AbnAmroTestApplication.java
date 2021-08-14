f4e1ee702fe4b0e746b460e866ae02c7df4e6d80
Santhosh <santhosh83@gmail.com>
2021-08-14 23:09:36 +1000

    Initial commit

---
+++ src/main/java/com/santhosh/abnamro/AbnAmroTestApplication.java
@@ -0,0 +1,13 @@
+package com.santhosh.abnamro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbnAmroTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbnAmroTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santhosh/abnamro/AbnAmroTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.abnamro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbnAmroTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
